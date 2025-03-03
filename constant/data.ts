67162fdd432ac0ff840ec730a27704fb49fce603
yuslove1 <adexyuslove@gmail.com>
2025-03-03 06:26:07 -0800

    static coffee data

---
+++ constant/data.ts
@@ -0,0 +1,60 @@
+const coffeeData = [
+    {
+      id: 1,
+      imageUrl: require("@/assets/images/coffee-1.png"),
+      title: "Caffè Mocha",
+      description: "Deep Foam",
+      price: 4.53,
+      rating: 4.8,
+    },
+    {
+      id: 2,
+      imageUrl: require("@/assets/images/coffee-2.png"),
+      title: "Flat White",
+      description: "Espresso",
+      price: 3.53,
+      rating: 4.5,
+    },
+    {
+      id: 3,
+      imageUrl: require("@/assets/images/coffee-3.png"),
+      title: "Caffè Mocha",
+      description: "Deep Foam",
+      price: 4.8,
+      rating: 4.8,
+    },
+    {
+        id: 4,
+        imageUrl: require("@/assets/images/coffee-4.png"), // Replace with your image path
+        title: "Americano",
+        description: "Espresso",
+        price: 3.8,
+        rating: 4.3,
+      },
+      {
+        id: 5,
+        imageUrl: require("@/assets/images/coffee-5.png"), // Replace with your image path
+        title: "Cappuccino",
+        description: "With Milk Foam",
+        price: 4.2,
+        rating: 4.6,
+      },
+      {
+        id: 6,
+        imageUrl: require("@/assets/images/coffee-6.png"), // Replace with your image path
+        title: "Latte",
+        description: "Espresso",
+        price: 3.9,
+        rating: 4.4,
+      },
+      {
+        id: 7,
+        imageUrl: require("@/assets/images/coffee-4.png"), // Replace with your image path
+        title: "Macchiato",
+        description: "Deep Foam",
+        price: 4.5,
+        rating: 4.7,
+      },
+  ];
+
+  export {coffeeData};
