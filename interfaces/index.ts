546b32c91907efa56d4c5f96180104cccf02df63
yuslove1 <adexyuslove@gmail.com>
2025-03-03 06:25:36 -0800

    App typescript interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,7 +1,31 @@
 export interface ButtonProps {
     text: string;
-    btnStyle?: string; // Tailwind className for TouchableOpacity
-    textStyle?: string;      // Tailwind className for Text
+    btnStyle?: string; 
+    textStyle?: string;      
+    onPress?: () => void;
+  }
+export interface IconProps {
+    imgSource: any;
+    imgStyle?: string; 
+    iconBodyStyle?: string;   
     onPress?: () => void;
   }
 
+  export interface CoffeeCardProps {
+    image: any; // Image source
+    title: string;
+    subtitle: string;
+    price: string;
+    rating: number;
+    onPress?: (coffee: Coffee) => void;
+  }
+
+ //for the coffee value padding throug the onPress
+  type Coffee = {
+    id: string;
+    imageUrl: string;
+    title: string;
+    description: string;
+    price: number;
+    rating: number;
+  };
