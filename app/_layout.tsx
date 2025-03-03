cece2fe1dc306959e21a70e01c9aa31480b040bf
yuslove1 <adexyuslove@gmail.com>
2025-03-03 06:29:01 -0800

    feat: app stack layout config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
-import '../styles/global.css'
+import "@/styles/global.css";
 
 export default function RootLayout() {
   return (
-<Stack screenOptions={{headerShown: false}} />
-  )
-  
+    <Stack screenOptions={{ headerShown: false }}>
+      
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="detail/:id" />
+    </Stack>
+  );
 }
